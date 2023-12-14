3f76900b7e4fa6256b8ce5fafba2d999f0ee88bf
Zeliha Selcuk <zeliha777@hotmail.com>
2023-12-14 11:48:32 +0300

    first commit

---
+++ src/ortak.java
@@ -0,0 +1,20 @@
+public class ortak {
+    public static void main(String[] args) {
+
+
+
+
+        git init
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/ZelihaSelcuk/ortak.git
+        git push -u origin main
+}
+
+
+
+
+
+
+
